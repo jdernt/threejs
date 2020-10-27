8342fa3561f217838b45552e8088ca4235e8cb3e
Natalia <jdernt@gmail.com>
2020-10-27 16:40:37 +0400

    sphere added

--- src/js/threejs/ThreeEntryPoint.js
+++ src/js/threejs/ThreeEntryPoint.js
@@ -1,72 +1,47 @@
-// Import dependencies
+import React from 'react';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export default function ThreeEntryPoint(sceneRef) {
-  // Create Scene
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x282c34);
+  scene.background = new THREE.Color(0x000000);
 
-  // Define a camera, set it to fill the browser window and position it
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-  camera.position.z = 5;
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 10;
 
-  // Define a renderer, and set it to fill the browser window
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  // Get an element from the DOM and append renderer.domElement to it
   sceneRef.appendChild(renderer.domElement);
 
-  // Add controls, targetting the same DOM element
-  let controls = new OrbitControls(camera, sceneRef);
-  controls.target.set(0, 0, 0);
-  controls.rotateSpeed = 0.5;
-  controls.update();
+  let geometry = new THREE.SphereGeometry( 15, 64, 64 );
+  let texture = new THREE.TextureLoader().load('./img/pano_2.png')
+  let material = new THREE.MeshBasicMaterial({ map: texture })
+  const sphere = new THREE.Mesh(geometry, material);
 
-  // Define (or import) your object's geometry
-  const geometry = new THREE.TorusKnotGeometry(10, 1.3, 500, 6, 6, 20);
+  sphere.scale.x = 0.1
+  sphere.scale.y = 0.1
+  sphere.scale.z = 0.1
 
-  // Define your object's material
-  const material = new THREE.MeshStandardMaterial({
-    color: 0xfcc742,
-    emissive: 0x111111,
-    specular: 0xffffff,
-    metalness: 1,
-    roughness: 0.55,
-  });
+  scene.add(sphere);
 
-  // Create the mesh, scale it and add it to the scene
-  const mesh = new THREE.Mesh(geometry, material);
+  geometry = new THREE.PlaneGeometry(10, 10)
+  material = new THREE.MeshNormalMaterial();
+  const plane = new THREE.Mesh(geometry, material);
+  plane.rotation.x = THREE.MathUtils.degToRad(-80)
+  plane.rotation.z = THREE.MathUtils.degToRad(-30)
+  plane.position.y = -1.5;
+  scene.add(plane)  
 
-  mesh.scale.x = 0.1;
-  mesh.scale.y = 0.1;
-  mesh.scale.z = 0.1;
-
-  scene.add(mesh);
-
-  // Create lights, position them, and add them to the scene
-  const frontSpot = new THREE.SpotLight(0xeeeece);
-  const frontSpot2 = new THREE.SpotLight(0xddddce);
-
-  frontSpot.position.set(1000, 1000, 1000);
-  frontSpot2.position.set(-500, -500, -500);
-
-  scene.add(frontSpot);
-  scene.add(frontSpot2);
-
-  // Create an animate function, which will allow you to render your scene and define any movements
   const animate = function () {
     requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.005;
-    mesh.rotation.y += 0.005;
-    mesh.rotation.z += 0.005;
+    // sphere.rotation.x += 0.005
+    sphere.rotation.y += 0.005
+    // sphere.rotation.z += 0.005
 
     renderer.render(scene, camera);
   };
 
-  // Call the animate function
   animate();
-
 }
