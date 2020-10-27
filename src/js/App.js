daf19cf2ba96742bf5e0c3e0db72d5aec72ed4ff
Natalia <jdernt@gmail.com>
2020-10-27 15:26:46 +0400

    initial commit

---
+++ src/js/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ThreeContainer from './components/ThreeContainer';
+
+function App() {
+  return (
+    <div>
+      <ThreeContainer />
+    </div>
+  );
+}
+
+export default App;
